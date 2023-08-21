0f93bdfb895a60281b2121f2881274617b18111b
cieun <129525965+cieun@users.noreply.github.com>
2023-08-22 03:14:46 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let img;
+
+function preload(){
+  img = loadImage('window1-1.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+  background(0,255,54);
+
+  //월드를 움직이도록 드래그
+  orbitControl();
+
+  normalMaterial();
+
+  
+  translate(0,0,-50);
+  for(let i=0; i<100; i++){
+    translate (0,0,i);
+    push();
+    texture(img);
+    plane(i*5);
+    pop();
+  }
+
+}
